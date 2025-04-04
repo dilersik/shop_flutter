826bd01cb123b083e3e55ebc710406355b67388f
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-04 16:06:05 -0300

    215. Criando Modelo Produto

---
+++ lib/models/product.dart
@@ -0,0 +1,31 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      price: (json['price'] as num).toDouble(),
+      imageUrl: json['imageUrl'] as String,
+    );
+  }
+
+  void toggleFavorite() {
+    isFavorite = !isFavorite;
+  }
+}
