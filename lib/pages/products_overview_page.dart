02c03418416d88142028738f5788db449515d73d
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-06 12:20:18 -0300

    217. Componente Item de Produto

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_flutter/data/dummy_data.dart';
 import 'package:shop_flutter/models/product.dart';
+import 'package:shop_flutter/widgets/product_item.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   final List<Product> loadedProducts = dummyProducts;
@@ -22,18 +23,16 @@ class ProductsOverviewPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: GridView.builder(
-          itemCount: loadedProducts.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemBuilder: (ctx, index) => Text(loadedProducts[index].title),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
         ),
+        itemBuilder: (ctx, index) => ProductItem(product: loadedProducts[index]),
       ),
     );
   }
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:shop_flutter/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(product.title, textAlign: TextAlign.center),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Add to cart functionality
+            },
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+        child: Image.network(product.imageUrl, fit: BoxFit.cover),
+      ),
+    );
+  }
+}
