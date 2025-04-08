3e0901fcdd5230fea42c12146a51c085116010d7
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-08 11:43:15 -0300

    230. Filtrar Favoritos #01

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,21 +5,34 @@ import 'package:shop_flutter/models/product.dart';
 
 class ProductList with ChangeNotifier {
   final List<Product> _products = dummyProducts;
-
-  List<Product> get products => [..._products]; // clone
-
-  void addProduct(Product product) {
-    _products.add(product);
-    notifyListeners();
+  List<Product> get products {
+    return _showFavoritesOnly ? _products.where((product) => product.isFavorite).toList() : [..._products]; // clone
   }
 
-  void removeProduct(Product product) {
-    _products.remove(product);
-    notifyListeners();
-  }
+    bool _showFavoritesOnly = false;
 
-  void clearProducts() {
-    _products.clear();
-    notifyListeners();
-  }
+    void addProduct(Product product) {
+      _products.add(product);
+      notifyListeners();
+    }
+
+    void removeProduct(Product product) {
+      _products.remove(product);
+      notifyListeners();
+    }
+
+    void clearProducts() {
+      _products.clear();
+      notifyListeners();
+    }
+
+    void showFavoritesOnly() {
+      _showFavoritesOnly = true;
+      notifyListeners();
+    }
+
+    void showAll() {
+      _showFavoritesOnly = false;
+      notifyListeners();
+    }
 }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../models/product_list.dart';
 import '../widgets/product_grid_widget.dart';
+
+enum FilterOptions {
+  favorites,
+  all,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('MyShop'),
         actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder:
+                (_) => [
+                  PopupMenuItem(value: FilterOptions.favorites, child: Text("Favorites only")),
+                  PopupMenuItem(value: FilterOptions.all, child: Text("Show all")),
+                ],
+            onSelected: (FilterOptions value) {
+              if (value == FilterOptions.favorites) {
+                provider.showFavoritesOnly();
+              } else {
+                provider.showAll();
+              }
+            }
+          ),
           IconButton(
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
-              // Navigator.of(context).pushNamed(CartScreen.routeName);
+              // Navigate to cart
             },
           ),
         ],
