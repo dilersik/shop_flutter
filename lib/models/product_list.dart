aa44d577c828a84359498c5c685495dba6c592f2
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-16 12:39:32 -0300

    285. Exclusão Otimista #01

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -5,15 +5,20 @@ import 'package:http/http.dart' as http;
 import 'package:shop_flutter/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _BASE_URL = 'https://shop-flutter-8d4e3-default-rtdb.firebaseio.com';
+  final _baseUrl = 'https://shop-flutter-8d4e3-default-rtdb.firebaseio.com';
 
   final List<Product> _items = [];
 
   List<Product> get items => [..._items]; // clone
   List<Product> get favorites => _items.where((product) => product.isFavorite).toList();
 
+  int get itemsCount => _items.length;
+
+  Uri _parseUrlWith(Product product) => Uri.parse('$_baseUrl/products/${product.id}.json');
+  Uri _parseUrl() => Uri.parse('$_baseUrl/products.json');
+
   Future<void> loadProducts() async {
-    final response = await http.get(Uri.parse('$_BASE_URL/products.json'));
+    final response = await http.get(_parseUrl());
     if (response.body == 'null') return;
 
     final Map<String, dynamic> data = jsonDecode(response.body);
@@ -35,7 +40,7 @@ class ProductList with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    final response = await http.post(Uri.parse('$_BASE_URL/products.json'), body: product.toJson());
+    final response = await http.post(_parseUrl(), body: product.toJson());
     final id = jsonDecode(response.body)['name'];
 
     _items.add(Product(
@@ -52,7 +57,7 @@ class ProductList with ChangeNotifier {
     final index = _items.indexWhere((p) => p.id == product.id);
     if (index >= 0) {
       await http.patch(
-        Uri.parse('$_BASE_URL/products/${product.id}.json'),
+        _parseUrlWith(product),
         body: product.toJson(),
       );
       _items[index] = product;
@@ -60,15 +65,16 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  void removeProduct(Product product) {
+  Future<void> removeProduct(Product product) async {
     final index = _items.indexWhere((p) => p.id == product.id);
     if (index >= 0) {
-      _items.removeWhere((p) => p.id == product.id);
+      final response = await http.delete(_parseUrlWith(product));
+      if (response.statusCode < 400) {
+        _items.removeAt(index);
+      }
       notifyListeners();
     }
   }
-
-  int get itemsCount => _items.length;
 }
 
 // List<Product> get products {
