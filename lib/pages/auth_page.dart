a807fdb664a22139d02386220885bb45e268e16a
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-17 14:20:28 -0300

    297. Tela de Autenticação #02

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -11,55 +11,51 @@ class AuthPage extends StatelessWidget {
           Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 255, 128, 0),
-                  Color.fromARGB(255, 255, 0, 0),
-                ],
+                colors: [Color.fromARGB(255, 255, 128, 0), Color.fromARGB(255, 255, 0, 0)],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
               ),
             ),
           ),
-          Center(
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              elevation: 8,
-              child: Container(
-                height: 300,
-                width: 300,
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text('Authenticate'),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: const InputDecoration(labelText: 'E-Mail'),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(height: 10),
-                    TextField(
-                      decoration: const InputDecoration(labelText: 'Password'),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-
-
-                      },
-                      child: const Text('Login'),
-                    ),
-                  ],
+          SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'MyShop',
+                  style: TextStyle(fontSize: 45, fontFamily: 'Anton', color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-              ),
+                const SizedBox(height: 20),
+                Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  elevation: 8,
+                  child: Container(
+                    width: 300,
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text('Authenticate', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                        const SizedBox(height: 20),
+                        TextField(
+                          decoration: const InputDecoration(labelText: 'E-Mail'),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(decoration: const InputDecoration(labelText: 'Password'), obscureText: true),
+                        const SizedBox(height: 20),
+                        ElevatedButton(onPressed: () {}, child: const Text('Login')),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
       ),
     );
   }
-
 }
