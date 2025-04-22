6247584f9adec172ba2bcd25a090c0730d9bbb9d
Dilermando Augusto Sikora <di_ler@hotmail.com>
2025-04-22 16:48:48 -0300

    backup

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,27 @@
 class Constants {
-  static const baseUrl = '';
-  static const apiKey = '';
+  static const baseUrl = 'https://shop-flutter-8d4e3-default-rtdb.firebaseio.com';
+  static const apiKey = 'AIzaSyAGZvMYXrwQ8cQDfQ27LZdgMbsVk0Y43x8'; // deleted
+  // Realtime Firebase Database rules
+// {
+//   "rules": {
+//     "orders": {
+//       "$uid": {
+//         ".write": "$uid === auth.uid",
+//         ".read": "$uid === auth.uid",
+//       },
+//     },
+//     "user-favorites": {
+//       "$uid": {
+//         ".write": "$uid === auth.uid",
+//         ".read": "$uid === auth.uid",
+//       },
+//     },
+//     "products": {
+//       ".write": "auth != null",
+//       ".read": "auth != null",
+//     }
+//   }
+// }
   static const firebaseAuthUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:';
   static const userFavoriteBaseUrl = '$baseUrl/user-favorites';
   static const productBaseUrl = '$baseUrl/products';
